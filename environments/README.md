5536506f7a5a2d09ff6d3831014a5a335cbbde74
LaurensRutten1997 <laurensrutten23@gmail.com>
2025-04-30 10:27:15 +0200

    Add playroom container to play against an opponent

--- environments/README.md
+++ environments/README.md
@@ -11,7 +11,9 @@ Each game should have the follwing fuctions or attributes
 - `action_space`: A gym space object that describes the possible options
 - `observation_space`: A gyme space object that describes the possible observations (i.e. game_state)
 - `game_state`: The gamestate defined in the observation state
+- `num_players`: The amount of players that play the game
+- `player_turn`: The player which turn it is
+- `possible_actions`: For each state these are the actions that are allowed at this point. If the players uses an action that is not possible he auto-loses the game
 - `reward`: The reward given to the player (-1 for lose, 0 if the game is still going, 1 for win)
 - `terminal`: Boolean to check whether the game as entered an terminal state
 - `winner`: Get the player that won the game, 0 if the game ends in a draw
-- `_possible_actions`: For each state these are the actions that are allowed at this point. If the players uses an action that is not possible he auto-loses the game
--- environments/game.py
+++ environments/game.py
@@ -1,5 +1,50 @@
 from gym import Env
+from random import randint, sample
 
 class Game(Env):
   def __init__(self):
     super().__init__()
+    self.num_players = 0
+    self.player_turn = 0
+    self.possible_actions = []
+    self.game_state = 0
+    self.reward = 0
+    self.terminal = False
+
+class PlayRoom(Env):
+  def __init__(self, game: Game, opponent: str = 'random'):
+    super().__init__()
+    self.game = game
+    self.opponent = opponent
+    self.action_space = self.game.action_space
+    self.observation_space = self.game.observation_space
+    self.player_turn = 0
+  
+  def reset(self):
+    self.player_turn = randint(1, self.game.num_players)
+    obs = self.game.reset()
+    while self.player_turn != self.game.player_turn:
+      obs, _, _, _, _ = self._take_opponent_turn()
+    return obs, {}
+  
+  def step(self, action):
+    obs, reward, terminated, truncated, info = self.game.step(action)
+    
+    #Check if game is finished
+    if terminated or truncated:
+      return obs, reward, terminated, truncated, info
+    
+    #Check if the player turn
+    while self.player_turn != self.game.player_turn:
+      obs, reward, terminated, truncated, info = self._take_opponent_turn()
+      #Check if game is finished
+      if terminated or truncated:
+        self.game.player_turn = self.player_turn
+        return self.game.game_state, self.game.reward, self.game.terminal, False, {}
+    
+    return obs, reward, terminated, truncated, info
+
+  def _take_opponent_turn(self):
+    if self.opponent == 'random':
+      action = sample(self.game.possible_actions)
+    return self.game.step(action)
